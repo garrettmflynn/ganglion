a52d4c025c4c494e127922a6b86d20373af69609
Garrett <gflynn@usc.edu>
2022-03-22 16:43:31 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as ganglion from './device.utils'
 
 export const device =  {
     // Generic 
-    label: 'Ganglion', 
+    label: 'ganglion', 
     device: ganglion.device,
     onconnect: ganglion.onconnect,
     protocols: ['bluetooth'],
